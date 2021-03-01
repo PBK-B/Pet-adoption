367ab137c590ce4beac0f9daf29e92b818b753c4
PBK-Bin <bin@PBK6.cn>
2021-03-01 23:07:40 +0800

    clear: Clear A useless import

--- app/src/main/java/com/example/androiddevchallenge/DetailsActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/DetailsActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.frames.modelMapOf
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.mutableStateOf
@@ -20,13 +19,11 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.example.androiddevchallenge.ui.theme.MyTheme
-import dev.chrisbanes.accompanist.coil.CoilImage
 import dev.chrisbanes.accompanist.glide.GlideImage
 
 class DetailsActivity : AppCompatActivity() {
--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -27,11 +27,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,7 +39,6 @@ import androidx.core.content.ContextCompat.startActivity
 
 import android.content.Intent
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
